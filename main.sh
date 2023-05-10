88467159fa0fb5f5dcae8fda67919df230a58575
faun3 <antonioculcea@gmail.com>
2023-05-10 22:24:33 +0300

    make menu text redisplay when choice is made

--- main.sh
+++ main.sh
@@ -3,14 +3,16 @@ PS3="Alegeti optiunea: "
 
 optiuni=("Creati fisier .csv" "Adauga inregistrare" "Sterge o inregistrare" "Actualizeaza o inregistrare")
 
-select optiune in "${optiuni[@]}" Iesire
-do
-    case $REPLY in
-        1) echo "Vom crea fisierul csv!";;
-        2) echo "Adaugam inregistrarea!";;
-        3) echo "Inregistrarea se va sterge permanent!";;
-        4) echo "Vom actualiza inregistrarea!";;
-        $((${#optiuni[@]}+1))) echo "Ok"; break;;
-        *) echo "Alegere invalida!";;
-    esac
+while true; do
+    select optiune in "${optiuni[@]}" Iesire
+    do
+        case $REPLY in
+            1) echo "Vom crea fisierul csv!"; break;;
+            2) echo "Adaugam inregistrarea!"; break;;
+            3) echo "Inregistrarea se va sterge permanent!"; break;;
+            4) echo "Vom actualiza inregistrarea!"; break;;
+            $((${#optiuni[@]}+1))) echo "Ok"; break 2;;
+            *) echo "Alegere invalida!"; break;;
+        esac
+    done
 done
