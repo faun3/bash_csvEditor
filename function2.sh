d4ff4b7cddd3a454860d0847b0c7dd3f037cd773
faun3 <antonioculcea@gmail.com>
2023-05-12 18:33:36 +0300

    write append function with auto numbering (no  regex field checking yet)

--- function2.sh
+++ function2.sh
@@ -4,6 +4,25 @@
     #we use "wc -l" to count the newlines in our file, adding 1 to the number 
     #   returned gives us our index
 
-echo "Apelat din function2"
+if [ -f data.csv ]
+then
+    echo "Vom adauga o noua inregistrare in fisierul date.csv"
+    echo
+    #only get the integer from the "wc -l" command
+    lineCount=$(wc -l < data.csv)
+    #compute new index
+    newEntryIndex=$((lineCount+1))
+
+    read -p "Introduceti numele: " newEntryName
+
+    read -p "Introduceti email-ul: " newEntryEmail
+
+    read -p "Introduceti nota: " newEntryGrade
+
+    echo >> data.csv
+    echo -n "$newEntryIndex,$newEntryName,$newEntryEmail,$newEntryGrade" >> data.csv
+else
+    echo "Fisierul cu date nu exista!"
+fi
 
 exit
