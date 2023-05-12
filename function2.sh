0c95463cff802e917ba3bd80470708f00e05f482
faun3 <antonioculcea@gmail.com>
2023-05-12 18:18:06 +0300

    test function2 being called from main

---
+++ function2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#code to set the index number of the new entry
+    #we use "wc -l" to count the newlines in our file, adding 1 to the number 
+    #   returned gives us our index
+
+echo "Apelat din function2"
+
+exit
