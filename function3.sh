6b27949e2e10cf33d6a1dbd456c20c4fce4431aa
faun3 <antonioculcea@gmail.com>
2023-05-12 22:16:53 +0300

    add long comment

--- function3.sh
+++ function3.sh
@@ -14,6 +14,10 @@ echo -n "id,name,email,grade" > copy.csv
 
 {
     read
+    # if we don't add this OR condition to our while loop the last line
+    #   is never read; read will fail upon reading the last line which
+    #   contains no data, however, data is loaded into the "remainingData" variable
+    #   so the OR check for it not being null will pass and the last line is read
     while IFS="," read -r id remainingData || [ -n "$remainingData" ]
     do
         if [[ $id != $deleteIndex ]]
