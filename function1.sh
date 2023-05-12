86f0956f42ccee6439ba7fa53e2bd28d397a5ee6
faun3 <antonioculcea@gmail.com>
2023-05-12 18:09:08 +0300

    finish create csv function

--- function1.sh
+++ function1.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
+if [ -f data.csv ]
+then
+    echo
+    echo "Fisierul .csv va fi recreat daca apasati 1!"
+    read -p 'Vreti sa rescrieti continutul fisierului? Apasati 1 pentru confirmare sau orice alta tasta pentru a refuza. ' choice
+    while true
+    do
+        case $choice in
+            1)
+                echo -n 'id,name,email,grade' > data.csv;
+                echo
+                echo 'Fisierul a fost recreat (contine doar capul de tabel) '
+                break 2;;
+            *)
+                break 2;;
+        esac
+    done
+else
+    echo
+    echo -n 'id,name,email,grade' > data.csv;
+    echo "Am creat fisierul .csv; ";
 
-echo "Apelat din main!"
+fi
+
 exit
