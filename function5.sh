23ca1db5359e109f4ac52faea7825ea1a2a78cc1
faun3 <antonioculcea@gmail.com>
2023-05-14 01:23:37 +0300

    almost make function5 work

--- function5.sh
+++ function5.sh
@@ -1,2 +1,39 @@
-echo "extragem elevii"
+
+#t sets the separator to a comma
+#the k argument specifies the column which we want to sort by
+#the n argument specifies that the sort is numeric
+    #bonus: numeric sorting will leave the header row untouched
+#the r argument reverses the sorting order
+    #this will move the header to the bottom, but we only want the first n rows anyway
+
+maxLines=$(wc -l < data.csv)
+regex='^([0-9])+$'
+
+read -p "Introduceti numarul de elevi: " lineNumber
+
+while true; do
+    if [[ $lineNumber -gt $maxLines ]]; then
+        read -p "Numarul introdus este mai mare decat numarul de inregistrari din fisier (${maxLines}). Apasati 1 pentru a introduce alta valoare sau orice alta tasta pentru a anula aceasta operatie de extragere: " choice
+        if [[ $choice == 1 ]]; then
+            read -p "Introduceti numarul de elevi: " lineNumber
+        else
+            break 2;
+            exit
+        fi
+    else
+        if ! [[ $lineNumber =~ $regex ]]; then
+            read -p "Valoarea introdusa nu pare sa fie un numar intreg. Apasati 1 pentru a introduce alta valoare sau orice alta tasta pentru a anula aceasta operatie de extragere: " choice
+            if [[ $choice == 1 ]]; then
+                read -p "Introduceti numarul de elevi: " lineNumber
+            else
+                break 2;
+                exit
+            fi
+        else
+            sort -t"," -k4 -n -r data.csv | head  -n $lineNumber
+            break 2;
+        fi
+    fi
+done
+
 exit
