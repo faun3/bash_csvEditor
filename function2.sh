981d3da2eeca68f965f771e41eac726f2ed4211a
faun3 <antonioculcea@gmail.com>
2023-05-14 22:42:06 +0300

    remove redundant comment

--- function2.sh
+++ function2.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-#code to set the index number of the new entry
-    #we use "wc -l" to count the newlines in our file, adding 1 to the number 
-    #   returned gives us our index
-
 if [ -f data.csv ]
 then
     echo "Vom adauga o noua inregistrare in fisierul date.csv"
